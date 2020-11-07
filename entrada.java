31e4b5b6bb510b399b3dd4c0fd7cb9bf9d399a91
manlio <manliot@uninorte.edu.co>
2020-11-07 12:39:29 -0500

    Correccion regla de los para, condicional y error con simbolo menos. listo las operaciones matematicas

--- entrada.java
+++ entrada.java
@@ -1,6 +1,7 @@
 public class Anything{
     public static void main (String[] args){
         int matriz[][] = {{2,4,4},{6,6,9},{8,10,12}};
+        int x=1-1;
         if(i && j==1){
             String profesional;
         }else{
@@ -11,10 +12,19 @@ public class Anything{
                     String profesional;
                     String profesional; 
                 }
-            }else{
+            }else
                 String profesional;
-            }
+            
         }
+        while(1==1){
+            String a= "hola terricola";
+            String a= "hola internauta";
+        }
+        for ( k = 1+1; k < j; k+=1+1+1){
+            i*=k;
+            z/=2.0;
+        }
+        int y=((1+2+(123+3))+(1+1))+(2-3);
         int y, x;
         int y=1,x=2;
         int yx;
