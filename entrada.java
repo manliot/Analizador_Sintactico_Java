966b2c2ddb0e6cddd157c20a78021239b1eb4b3b
manlio <manliot@uninorte.edu.co>
2020-11-07 12:59:30 -0500

    correcion regla gramatical del para

--- entrada.java
+++ entrada.java
@@ -1,8 +1,9 @@
 public class Anything{
     public static void main (String[] args){
         int matriz[][] = {{2,4,4},{6,6,9},{8,10,12}};
-        int x=1-1;
-        if(i && j==1){
+        int matriz[][] = new int [][];
+        char x=11+1+1;
+        if(i && j>=1){
             String profesional;
         }else{
             String profesional;
@@ -16,16 +17,34 @@ public class Anything{
                 String profesional;
             
         }
-        while(1==1){
+        while(i==1){
             String a= "hola terricola";
             String a= "hola internauta";
         }
-        for ( k = 1+1; k < j; k+=1+1+1){
+        for (  k = 1; k < j; k+=1){
             i*=k;
             z/=2.0;
         }
         int y=((1+2+(123+3))+(1+1))+(2-3);
         int y, x;
+        int y=1,x=2;
+        int yx;
+        int y;
+        int x;
+        float x=1.3;
+        float x=1.3;
+        x=x;
+        x="Hola mis amigos";
+        //int x = 2 + 2;    
+        int y=1,x=2;
+        int yx;
+        int y;
+        int x;
+        float x=1.3;
+        float x=1.3;
+        x=x;
+        x="Hola mis amigos";
+        //int x = 2 + 2;    
         int y=1,x=2;
         int yx;
         int y;
